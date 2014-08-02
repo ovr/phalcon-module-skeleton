1898d88e3c4bb7f22b74d9c53a2ad2cde0ed2fcd
Дмитрий Пацура <zaets28rus@gmail.com>
2014-08-02 06:12:22 +0400

    Implement demo login

--- application/modules/admin/controllers/AuthController.php
+++ application/modules/admin/controllers/AuthController.php
@@ -3,7 +3,12 @@
 namespace Admin\Controller;
 
 use Phalcon\Mvc\Controller;
+use Admin\Form\Login;
 
+/**
+ * Class AuthController
+ * @package Admin\Controller
+ */
 class AuthController extends Controller
 {
     public function initialize()
@@ -13,7 +18,25 @@ class AuthController extends Controller
 
     public function loginAction()
     {
-        $form = new \Admin\Form\Login();
+        $form = new Login();
+
+        if ($this->request->isPost()) {
+            try {
+                if ($form->isValid($this->request->getPost())) {
+                    /**
+                     * @todo Rewrite for AuthService with check
+                     */
+                    $this->session->start();
+                    $this->session->set('id', '1');
+
+                    $this->response->redirect(array('for' => 'admin'));
+                } else {
+
+                }
+            } catch (\Exception $e) {
+                $this->flash->error($e->getMessage());
+            }
+        }
 
         $this->view->form = $form;
     }
