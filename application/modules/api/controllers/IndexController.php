84baa2808ad8683022400ea4930604c0af148443
Дмитрий Пацура <zaets28rus@gmail.com>
2014-08-06 15:27:45 +0400

    [API] Index implement test

--- application/modules/api/controllers/IndexController.php
+++ application/modules/api/controllers/IndexController.php
@@ -1,10 +1,16 @@
 <?php
 
-namespace User\Controller;
+namespace Api\Controller;
 
 use Phalcon\Mvc\Controller;
 
 class IndexController extends Controller
 {
-
+    public function testAction()
+    {
+        return array(
+            'success' => true,
+            'version' => 1.0
+        );
+    }
 }
