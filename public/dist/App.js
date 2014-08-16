c3576fa3a5524f597904966cb2a3bc30590743cb
Дмитрий Пацура <zaets28rus@gmail.com>
2014-08-17 02:54:32 +0400

    Write ADM.App

---
+++ public/dist/App.js
@@ -0,0 +1,39 @@
+window.ADM = (function() {
+  function ADM() {}
+
+  return ADM;
+
+})();
+
+ADM.App = (function() {
+  function App() {}
+
+  App.prototype.events = _.extend(BBNS.Events, Backbone.Events);
+
+  App.prototype.init = function() {
+    this.events.t('init:start');
+    this.events.on('dom:onload', this.dom_onload, this);
+    return this.events.on('init:dom:end', function() {
+      return this.events.t('init:end');
+    }, this);
+  };
+
+  App.prototype.dom_onload = function() {};
+
+  return App;
+
+})();
+
+ADM.app = new ADM.App;
+
+ADM.app.events.on('router:init:end', function() {
+  return Backbone.history.start({
+    pushState: true
+  });
+});
+
+ADM.app.init();
+
+$(function() {
+  return BBNS.app.events.t('dom:onload');
+});
---
+++ public/src/app/App.coffee
@@ -0,0 +1,21 @@
+class window.ADM
+
+class ADM.App
+
+  events: _.extend(BBNS.Events, Backbone.Events)
+
+  init: ->
+    @events.t 'init:start'
+    @events.on 'dom:onload', @dom_onload, this
+
+    @events.on 'init:dom:end', ->
+      @events.t 'init:end'
+    , this
+
+  dom_onload: ->
+
+ADM.app = new ADM.App
+ADM.app.events.on 'router:init:end', -> Backbone.history.start pushState: true
+ADM.app.init()
+
+$ -> BBNS.app.events.t 'dom:onload'
