d1efd710e089024565fec8525b8b3600b9930685
Дмитрий Пацура <zaets28rus@gmail.com>
2014-09-13 14:11:48 +1000

    Added empty Composer\ScriptHandler

---
+++ library/Composer/ScriptHandler.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @author Patsura Dmitry @ovr <talk@dmtry.me>
+ */
+
+namespace App\Composer;
+
+use Composer\Script\Event;
+
+class ScriptHandler
+{
+    public static function build(Event $event)
+    {
+
+    }
+}
