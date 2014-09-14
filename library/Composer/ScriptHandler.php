0d3731eea14b76d524c8356c2158c57797e2c071
Дмитрий Пацура <zaets28rus@gmail.com>
2014-09-14 12:45:55 +1000

    Added installation system

--- library/Composer/ScriptHandler.php
+++ library/Composer/ScriptHandler.php
@@ -9,6 +9,44 @@ use Composer\Script\Event;
 
 class ScriptHandler
 {
+    /**
+     * Install skeleton
+     *
+     * @param Event $event
+     */
+    public static function install(Event $event)
+    {
+        $io = $event->getIO();
+
+        $io->write('Welcome to install of Phalcon modules skeleton');
+        $io->write('<comment>Some parameters are missing. Please provide them.</comment>');
+
+        $extra = $event->getComposer()->getPackage()->getExtra();
+        $appDir = realpath(__DIR__ . '/../../' . $extra['app-dir']);
+        $configDir = $appDir . '/config/';
+
+        $distConfigure = include_once $configDir . 'parameters.php.dist';
+
+        foreach ($distConfigure as $service => $requiredParamaters) {
+            $io->write(ucfirst($service) . ' service');
+
+            foreach ($requiredParamaters as $key => $value) {
+                if ($key == 'options') {
+                    continue;
+                }
+
+                $default = $value;
+                $value = $io->ask(sprintf('<question>%s</question> (<comment>%s</comment>): ', ucfirst($key), $default), $default);
+
+                if ($value != $default) {
+                    $distConfigure[$service][$key] = $value;
+                }
+            }
+        }
+
+        file_put_contents($configDir . 'parameters.php', '<?php ' . PHP_EOL . ' return ' . var_export($distConfigure, true) . ';' . PHP_EOL);
+    }
+
     public static function build(Event $event)
     {
 
