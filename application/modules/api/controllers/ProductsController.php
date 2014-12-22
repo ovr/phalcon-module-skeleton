97b1223071481e18a939c4aa8c198cbf07471f70
Дмитрий Пацура <talk@dmtry.me>
2014-12-22 16:32:25 +0900

    [API] Added ProductsController

---
+++ application/modules/api/controllers/ProductsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Api\Controller;
+
+use Phalcon\Mvc\Controller;
+use Phalcon\Paginator\Adapter\QueryBuilder;
+use Catalog\Model\Product;
+
+/**
+ * Class ProductsController
+ * @package Api\Controller
+ */
+class ProductsController extends Controller
+{
+    public function indexAction()
+    {
+        $limit = $this->request->get('limit', ['trim', 'int'], 100);
+        if ($limit <= 0) {
+            throw new \Exception('Limit must be > 0');
+        } else if ($limit > 500) {
+            throw new \Exception('Limit must be <= 500');
+        }
+
+        $page = $this->request->get('page', ['trim', 'int'], 1);
+        if ($page < 1) {
+            throw new \Exception('Page must be > 0');
+        }
+
+        $builder = $this->modelsManager->createBuilder()
+            ->from('Catalog\Model\Product')
+            ->orderBy('id DESC');
+
+        $paginator = new QueryBuilder(array(
+            "builder" => $builder,
+            "limit"=> $limit,
+            "page" => $page
+        ));
+
+
+        $result = array();
+        $page = $paginator->getPaginate();
+
+        /**
+         * @var $product Product
+         */
+        foreach ($page->items as $product) {
+            $result[] = [
+                'id' => $product->id,
+                'title' => $product->title
+            ];
+        }
+
+        return array(
+            'success' => true,
+            'result' => array(
+                'users' => $result,
+                'pages' => $page->total_pages,
+                'total' => $page->total_items
+            )
+        );
+    }
+
+    public function getAction($id)
+    {
+        if ($id <= 0) {
+            throw new \Exception('Wrong id passed', 500);
+        }
+
+        /**
+         * @var $entity Product|boolean
+         */
+        $entity = Product::findFirst($id);
+        if (!$entity) {
+            throw new \Exception('Product not found', 404);
+        }
+
+        return array(
+            'success' => true,
+            'result' => array(
+                'id' => $entity->id,
+                'title' => $entity->title
+            )
+        );
+    }
+}
