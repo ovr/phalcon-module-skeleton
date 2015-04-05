74084481e4e45484ced6e4fb7270f39aae586b7a
Дмитрий Пацура <talk@dmtry.me>
2015-04-05 14:03:58 +0900

    TravisCI install php5-mysql php5-memcache

--- tests/ci/install-fpm-nginx.sh
+++ tests/ci/install-fpm-nginx.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 sudo apt-get update -y
-sudo apt-get install -y nginx php5-fpm
+sudo apt-get install -y nginx php5-fpm php5-mysql php5-memcache
 sudo cp $TRAVIS_BUILD_DIR/tests/ci/phalcon-module.local.conf /etc/nginx/sites-enabled/phalcon-module.local.conf
 sudo service nginx restart
