6796d01f35a239fe3df90bf6084aa900ecff99e7
Дмитрий Пацура <talk@dmtry.me>
2015-04-05 13:54:38 +0900

    TravisCI trying to fix build

--- tests/ci/install-fpm-nginx.sh
+++ tests/ci/install-fpm-nginx.sh
@@ -2,5 +2,5 @@
 
 sudo apt-get update -y
 sudo apt-get install -y nginx php5-fpm
-sudo cp ./phalcon-module.local.conf /etc/nginx/sites-enabled/phalcon-module.local.conf
-sudo /etc/init.d/nginx restart
+sudo cp $TRAVIS_BUILD_DIR/tests/ci/phalcon-module.local.conf /etc/nginx/sites-enabled/phalcon-module.local.conf
+sudo service nginx restart
