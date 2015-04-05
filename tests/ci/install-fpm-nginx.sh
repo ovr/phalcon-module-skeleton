bfa1310f1926b8d995d21d5ba605d2477e1d45ec
Дмитрий Пацура <talk@dmtry.me>
2015-04-05 13:49:32 +0900

    TravisCI run apt-get update and fix path for conf

--- tests/ci/install-fpm-nginx.sh
+++ tests/ci/install-fpm-nginx.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+sudo apt-get update -y
 sudo apt-get install -y nginx php5-fpm
-sudo cp phalcon-module.local.conf /etc/nginx/sites-enabled/phalcon-module.local.conf
+sudo cp ./phalcon-module.local.conf /etc/nginx/sites-enabled/phalcon-module.local.conf
 sudo /etc/init.d/nginx restart
